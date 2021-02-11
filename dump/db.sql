361219ec2e82954a7270a8ef8fc1a1b43588759e
adagassama <adamagassama40.ag@gmail.com>
2021-02-11 13:03:31 +0100

    Update db.sql

--- dump/db.sql
+++ dump/db.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE IF NOT EXISTS testdatabase;
 USE testdatabase;
--- Création de la base de donnée
+
 CREATE TABLE `Personne` (
     `id` int(11) NOT NULL,
+    `prenom` varchar(20) NOT NULL,
     `nom` varchar(20) NOT NULL);
 INSERT INTO
-  `Personne` (`id`, `nom`)
+  `Personne` (`id`, `prenom`, `nom`)
 VALUES
-  (1, 'Adama'),
-  (2, 'Florian'),
-  (3, 'Sylvain');
+  (1, 'Adama', 'GASSAMA'),
+  (2, 'Florian', 'GUILLOT'),
+  (3, 'Sylvain', 'COUTROT'),
+  (4, 'Adama', 'GASSAMA'),
+  (5, 'Florian', 'GUILLOT');
