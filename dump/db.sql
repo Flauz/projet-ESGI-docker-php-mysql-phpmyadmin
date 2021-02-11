684a19087805b949dc3f5d55b3d3de18a3398700
adagassama <adamagassama40.ag@gmail.com>
2021-02-11 22:33:00 +0100

    Update db.sql

--- dump/db.sql
+++ dump/db.sql
@@ -1,15 +1,23 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
 CREATE DATABASE IF NOT EXISTS testdatabase;
 USE testdatabase;
 
-CREATE TABLE `Personne` (
-    `id` int(11) NOT NULL,
-    `prenom` varchar(20) NOT NULL,
-    `nom` varchar(20) NOT NULL);
+  CREATE TABLE IF NOT EXISTS `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 INSERT INTO
-  `Personne` (`id`, `prenom`, `nom`)
+  `Users` (`prenom`, `nom`, `password`)
 VALUES
-  (1, 'Adama', 'GASSAMA'),
-  (2, 'Florian', 'GUILLOT'),
-  (3, 'Sylvain', 'COUTROT'),
-  (4, 'Adama', 'GASSAMA'),
-  (5, 'Florian', 'GUILLOT');
+  ('Adama', 'GASSAMA', 'vide'),
+  ('Florian', 'GUILLOT', 'vide'),
+  ('Sylvain', 'COUTROT', 'vide');
+
